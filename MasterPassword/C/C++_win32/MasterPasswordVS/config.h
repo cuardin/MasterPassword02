01aee842cf9a19349a4fb67c84ba3f25328af9d2
cuardin <daniel@armyr.se>
2014-08-20 10:46:39 +0200

    Deleted old VS project

--- MasterPassword/C/C++_win32/MasterPasswordVS/config.h
+++
@@ -1,7 +0,0 @@
-/* A default configuration for FreeBSD, used if there is no config.h. */
-
-//None of these are actually referenced inside the any of the files used by MasterPassword. (I searched).
-//#define HAVE_POSIX_MEMALIGN 1
-//#define HAVE_SYSCTL_HW_USERMEM 1
-//#define HAVE_CLOCK_GETTIME 1
-//#define HAVE_SYS_PARAM_H 1
--- MasterPassword/C/C++_win32/MasterPasswordVS/mpw_core.h
+++
@@ -1,8 +0,0 @@
-#ifndef _MPW_CORE_H_
-#define _MPW_CORE_H_
-
-int mpw_core(char * const password, const size_t passLen, char const * const userName,
-	char const * const masterPassword, char const * const siteTypeString, char const * const siteName,
-	const int siteCounter);
-
-#endif
