c0a327c8a5aff2be4a68933d403ae979b9feb95b
cuardin <daniel@armyr.se>
2014-11-05 16:07:07 +0100

    Restructured to use single-source.

--- MasterPassword/C/emscripten/runme.bat
+++ MasterPassword/C/emscripten/runme.bat
@@ -1,1 +1,1 @@
-emcc helloWorld.c testutils.c types.c sha256.c crypto_scrypt-ref.c -s TOTAL_MEMORY=134217728 -s EXPORTED_FUNCTIONS="['_libcperciva_HMAC_SHA256_Buf', '_scrypt_wrapper']"
+emcc -I../src/ -I../src/scrypt/ helloWorld.c testutils.c ..\src\scrypt\sha256.c ..\src\scrypt\crypto_scrypt-ref.c -s TOTAL_MEMORY=134217728 -s EXPORTED_FUNCTIONS="['_libcperciva_HMAC_SHA256_Buf', '_scrypt_wrapper']" 
