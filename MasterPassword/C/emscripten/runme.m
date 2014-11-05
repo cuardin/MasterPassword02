2e84661abd6d77fc2b9a0ddb55bf1e2c28a81cad
Daniel Armyr <daniel.armyr@mathworks.com>
2014-11-05 14:23:18 +0100

    SHA256 now works when called from JavaScript

--- MasterPassword/C/emscripten/runme.m
+++ MasterPassword/C/emscripten/runme.m
@@ -1,4 +1,4 @@
-!emcc helloWorld.c testutils.c types.c sha256.c crypto_scrypt-ref.c -s TOTAL_MEMORY=134217728
+!emcc helloWorld.c testutils.c types.c sha256.c crypto_scrypt-ref.c -s TOTAL_MEMORY=134217728 -s EXPORTED_FUNCTIONS="['_libcperciva_HMAC_SHA256_Buf']"
 tic;
 !node a.out.js
 toc;
