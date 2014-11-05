b9e05bef399bf1e179e062d3b09fe447c539df7b
cuardin <daniel@armyr.se>
2014-11-05 16:09:44 +0100

    Renamed the output

--- MasterPassword/C/emscripten/runme.bat
+++ MasterPassword/C/emscripten/runme.bat
@@ -1,1 +1,1 @@
-emcc -I../src/ -I../src/scrypt/ helloWorld.c testutils.c ..\src\scrypt\sha256.c ..\src\scrypt\crypto_scrypt-ref.c -s TOTAL_MEMORY=134217728 -s EXPORTED_FUNCTIONS="['_libcperciva_HMAC_SHA256_Buf', '_scrypt_wrapper']" 
+emcc -I../src/ -I../src/scrypt/ helloWorld.c testutils.c ..\src\scrypt\sha256.c ..\src\scrypt\crypto_scrypt-ref.c -o scrypt-asm.js -s TOTAL_MEMORY=134217728 -s EXPORTED_FUNCTIONS="['_libcperciva_HMAC_SHA256_Buf', '_scrypt_wrapper']" 
