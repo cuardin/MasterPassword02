14a9681e64f80572325e342519b686ee18d3a899
cuardin <daniel@armyr.se>
2014-12-05 11:42:36 +0100

    Deleted emscripten and polyspace projects.

--- MasterPassword/C/emscripten/testutils.h
+++
@@ -1,9 +0,0 @@
-#ifndef _TESTUTILS_H_
-#define _TESTUTILS_H_
-
-#include "types.h"
-
-void convertFromHex(char const * const input, void * const output, const size_t outputLength);
-const char *Hex(const void *buf, size_t length);
-
-#endif
